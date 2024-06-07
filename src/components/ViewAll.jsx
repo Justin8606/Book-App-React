fba854a9388aca05c43f372fefc5dcb902b35d97
Justin8606 <justinshajuk@gmail.com>
2024-06-07 13:50:56 +0530

    View All

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+
+const ViewAll = () => {
+  return (
+    <div>
+
+<table class="table table-dark table-bordered  border-light">
+        <thead>
+          <tr>
+            
+            <th scope="col">Book Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Published Year</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          <tr>
+            
+            <td>Book1</td>
+            <td>Amal</td>
+            <td>2004</td>
+            <td>757</td>
+
+          </tr>
+          
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ViewAll
